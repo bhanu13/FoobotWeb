574ff5502daf0963b8c06d63b6cba72a05a6758c
Bhanu Agarwal <bhanu.agarwal.13@gmail.com>
2015-07-15 19:58:22 +0530

    Updated Readme

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from foobot import *
 
 
-
-magnetobot = FoobotWeb("magnetog@gmail.com")
+email = str(raw_input("Enter your email ID:\n"))
+magnetobot = FoobotWeb(email)
 
 # magnetobot.GetDatapointLast()
 
 # magnetobot.GetDatapointLast(3600, 0)
 starttime = datetime(year=2015, month=1, day=1, hour=0, minute=0, second=0)
 # endtime = datetime(year=2015, month=7, day=15, hour=0, minute=0, second=0)
-magnetobot.GetDataInterval(starttime = starttime, save = True)	# endtime defaulting to now
+# magnetobot.GetDataInterval(starttime = starttime, save = True)	# endtime defaulting to now
 
-# magnetobot.GetLastHour(save = True)
+magnetobot.GetLastHour(save = True)
 
 # magnetobot.GetLastDay(save = True)
